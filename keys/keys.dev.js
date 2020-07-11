61208462f8a2f19a57b9100ccc8dc928b57f78de
GrigoriTrushin <58602345+GrigoriTrushin@users.noreply.github.com>
2020-07-11 18:59:23 +0300

    Add files via upload

---
+++ keys/keys.dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  MONGODB_URI: 'mongodb+srv://training:Zj705VvlX0AtKlx9@cluster0.4mgpd.mongodb.net/shop',
+  SESSION_SECRET: 'some secret value',
+  SENDGRID_API_KEY: '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',
+  EMAIL_FROM: 'nodejs@email.ru',
+  BASE_URL: 'http://localhost:3000'
+}
